f3be5c2985c026c1e7ddaef4459cf2733b6a1325
Katieyeh <74290025+Katieyeh@users.noreply.github.com>
2020-12-07 15:50:47 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function myFunction(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg");
+    // Get the image text
+    var imgText = document.getElementById("imgtext");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+}
+function myFunction2(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg2");
+    // Get the image text
+    var imgText = document.getElementById("imgtext2");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+}
+function myFunction3(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg3");
+    // Get the image text
+    var imgText = document.getElementById("imgtext3");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+}
+
+function showMeg(){
+    alert("感謝您的寶貴意見，我將盡快與您聯繫😊")
+
+}
